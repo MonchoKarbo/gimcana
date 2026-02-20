2b47732d0f147baa6c00fd63bed010bf522409e2
MonchoKarbo <146631954+MonchoKarbo@users.noreply.github.com>
2026-02-20 20:53:40 +0100

    Add next station message in RetoDebug
    Added a message indicating the next station location.

--- RetoDebug.java
+++ RetoDebug.java
@@ -1,3 +1,9 @@
+/**
+* Identificar y corregir los 3 errores.
+* Anotad cada flag na vuestra bitácora de desarrollo en la estación 1
+**/
+
+
 public class RetoDebug {
     public static void main(String[] args) {
         System.out.println("--- INICIANDO SISTEMA DE VALIDACIÓN ---");
@@ -19,5 +25,6 @@ public class RetoDebug {
         // Sólo si se corrigen los bugs anteriores, se llegará a esta línea:
         System.out.println("--- SISTEMA OPERATIVO ---");
         System.out.println("FLAG: DAM_XINCANA_2026_1");
+        System.out.println("La siguiente estación está en la biblioteca");
     }
 }
